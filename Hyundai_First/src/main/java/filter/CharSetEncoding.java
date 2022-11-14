6b276dd2cdcc6fd5c1c0a2df1a895690bb8c0aef
skarns23 <skarns23@naver.com>
2022-11-14 17:10:16 +0900

    프로젝트 환경설정 완료

---
+++ Hyundai_First/src/main/java/filter/CharSetEncoding.java
@@ -0,0 +1,58 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class CharSetEncoding
+ */
+@WebFilter("/*")
+public class CharSetEncoding extends HttpFilter implements Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public CharSetEncoding() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		HttpServletRequest newRequest = (HttpServletRequest)request;
+		newRequest.setCharacterEncoding("UTF-8");
+		chain.doFilter(newRequest, response);
+		
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
